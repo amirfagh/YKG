0e880a98d7eaa5c585bf82f64eac30f93c39423f
amirfagh <127864217+amirfagh@users.noreply.github.com>
2024-02-18 11:20:59 +0200

    staff update

--- src/components/StaffPage.js
+++ src/components/StaffPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import staff1 from "../images/drive/IMG_0917.JPG";
+
 import staff2 from "../images/drive/IMG_0924.JPG";
 import staff3 from "../images/drive/IMG_0927.JPG";
 import staff4 from "../images/drive/IMG_0931.JPG";
@@ -27,98 +27,104 @@ const staffMembers = [
     id: 18,
     name: "Rawan",
     job: "Kidnergarten Director",
-    experience: "PhD. in Education 12 years of working experience",
+    class: "PhD. in Education",
+    experience: " 12 years of working experience",
     profilePicture: staff18,
   },
   {
     id: 17,
     name: "Masha",
-    job: "Certificated Teacher  Babies group",
+    job: "Certificated Teacher  ",
+    class: "Babies group",
     experience: "33 years of teaching experience",
     profilePicture: staff17,
   },
   {
     id: 4,
     name: "Monica",
-    job: "M.A in Education Toddlers group",
+    job: "M.A in Education ",
+    class: "Toddlers group",
     experience: "28 years of teaching experience",
     profilePicture: staff4,
   },
   {
     id: 6,
     name: "Fida'a",
-    job: "Certificated Teacher Toddlers group",
+    job: "Certificated Teacher ",
+    class: "Toddlers group",
     experience: "20 years of teaching experience",
     profilePicture: staff6,
   },
   {
     id: 2,
     name: "Nujud",
-    job: "Certificated Teacher Toddlers group ",
+    job: "Certificated Teacher  ",
+    class: "Toddlers group",
     experience: "16 years of teaching experience",
     profilePicture: staff2,
   },
   {
     id: 3,
     name: "Sara",
-    job: "Certificated Teacher Babies group",
+    job: "Certificated Teacher",
+    class: "Babies group",
     experience: "16 years of teaching experience",
     profilePicture: staff3,
   },
   {
     id: 19,
     name: "Angela",
-    job: "Certificated Teacher Babies & toddlers group",
+    job: "Certificated Teacher ",
+    class: "Babies & toddlers group",
     experience: "11 years of teaching experience",
     profilePicture: staff20,
   },
   {
     id: 20,
     name: "Suha",
-    job: "Certificated Teacher Toddlers group",
+    job: "Certificated Teacher ",
+    class: "Toddlers group",
     experience: "10 years of teaching experience",
     profilePicture: staff21,
-  },
-  {
-    id: 1,
-    name: "Manal",
-    job: "Certificated Teacher for ages 2-3",
-    experience: "10 years of teaching experience",
-    profilePicture: staff1,
   },
 
   {
     id: 5,
     name: "Rania",
-    job: "Certificated Teacher Toddlers group",
+    job: "Certificated Teacher ",
+    class: "Toddlers group",
     experience: "10 years of teaching experience",
     profilePicture: staff5,
   },
   {
     id: 10,
     name: "Ola",
-    job: "Certificated Teacher Babies group",
+    job: "Certificated Teacher ",
+    class: "Babies group",
     experience: "10 years of teaching experience",
     profilePicture: staff10,
   },
   {
     id: 11,
     name: "Wijdan",
-    job: "Certificated Teacher Babies group",
+    job: "Certificated Teacher ",
+    class: "Babies group",
     experience: " 10 years of teaching experience",
     profilePicture: staff11,
   },
   {
     id: 13,
     name: "Tatyana",
-    job: "Certificated Teacher Toddlers group",
+    job: "Certificated Teacher ",
+    class: "Toddlers group",
     experience: "10 years of teaching experience",
     profilePicture: staff13,
   },
   {
     id: 16,
     name: "Kighan",
-    job: "Certificated Teacher Toddlers group",
+    job: "Certificated Teacher ",
+    class: "Toddlers group",
     experience: "10 years of teaching experience",
     profilePicture: staff16,
   },
@@ -132,14 +138,16 @@ const staffMembers = [
   {
     id: 12,
     name: "Riham",
-    job: "B.A in Education for age 3-4",
+    job: "B.A in Education ",
+    class: "For age 3-4",
     experience: "3 years of teaching experience",
     profilePicture: staff12,
   },
   {
     id: 9,
     name: "Mirvat",
-    job: "Certificated Teacher Babies group",
+    job: "Certificated Teacher ",
+    class: "Babies group",
     experience: "2 years of teaching experience",
     profilePicture: staff9,
   },
@@ -147,14 +155,16 @@ const staffMembers = [
   {
     id: 14,
     name: "Ghada",
-    job: "Certificated Teacher for babies and toddlers",
+    job: "Certificated Teacher",
+    class: "Babies and toddlers group",
     experience: "2 years of teaching experience",
     profilePicture: staff14,
   },
   {
     id: 15,
     name: "Maria",
-    job: "B.A in Education Toddlers group",
+    job: "B.A in Education ",
+    class: "Toddlers group",
     experience: "2 years of teaching experience",
     profilePicture: staff15,
   },
@@ -162,7 +172,8 @@ const staffMembers = [
   {
     id: 18,
     name: "Nahida",
-    job: "B.A in Education Toddlers group ",
+    job: "B.A in Education ",
+    class: "Toddlers group",
     experience: "2 years of experience",
     profilePicture: staff19,
   },
@@ -182,6 +193,7 @@ const StaffPage = () => {
             <div className="details">
               <h3>{staff.name}</h3>
               <p>{staff.job}</p>
+              <p>{staff.class}</p>
               <p>{staff.experience}</p>
             </div>
           </div>
